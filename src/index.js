32a86b0b3b016b46bf77ba86c038f1271618b3fd
mengmeng183 <1911815277@qq.com>
2016-09-19 08:23:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Routers from './routes';
 // import Marked from './Marked';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'highlight.js/styles/arduino-light.css';
+import 'highlight.js/styles/atelier-seaside-dark.css';
 // import './main.css';
 
 ReactDOM.render(<Routers />, document.getElementById('app'));
